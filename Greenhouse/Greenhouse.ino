87886ffdd1378eb58e9491a4f241f384fccc7560
aluna25 <48074319+aluna25@users.noreply.github.com>
2019-04-24 16:20:54 -0500

    Update Greenhouse.ino

--- Greenhouse/Greenhouse.ino
+++ Greenhouse/Greenhouse.ino
@@ -1,9 +1,17 @@
 #include <dht.h>
 
+const int sensorPin = A1;
+const int ledPin = 9;
 const int temperaturePin = 0;
 int val = 0; //value for storing moisture value 
-int soilPin = A0;//Declare a variable for the soil moisture sensor 
+const int soilPin = A0;//Declare a variable for the soil moisture sensor 
 int soilPower = 1;//Variable for Soil moisture Power
+
+// We'll also set up some global variables for the light level:
+int lightLevel;
+int calibratedlightLevel; // used to store the scaled / calibrated lightLevel
+int maxThreshold = 0;     // used for setting the "max" light level
+int minThreshold = 1023;   // used for setting the "min" light level
 
 dht DHT;
 #define DHT11_PIN 7
@@ -11,44 +19,25 @@ dht DHT;
 void setup() {
   Serial.begin(9600);
 
+  pinMode(sensorPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);    // Set up the LED pin to be an output.
   pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
   digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
 }
 
 void loop() {
+  
   temp_check();
   delay(1000);
   soil_moisture();
-//  delay(1000);
+  delay(1000);
+  photoResistor();
+  delay(1000);
+
 }
 
 float getVoltage(int pin)
 {
-  // This function has one input parameter, the analog pin number
-  // to read. You might notice that this function does not have
-  // "void" in front of it; this is because it returns a floating-
-  // point value, which is the true voltage on that pin (0 to 5V).
-
-  // You can write your own functions that take in parameters
-  // and return values. Here's how:
-
-    // To take in parameters, put their type and name in the
-    // parenthesis after the function name (see above). You can
-    // have multiple parameters, separated with commas.
-
-    // To return a value, put the type BEFORE the function name
-    // (see "float", above), and use a return() statement in your code
-    // to actually return the value (see below).
-
-    // If you don't need to get any parameters, you can just put
-    // "()" after the function name.
-
-    // If you don't need to return a value, just write "void" before
-    // the function name.
-
-  // Here's the return statement for this function. We're doing
-  // all the math we need to do within this statement:
-
   return (analogRead(pin) * 0.004882814);
 
   // This equation converts the 0 to 1023 value that analogRead()
@@ -86,4 +75,40 @@ void soil_moisture(){
 //  delay(1000);//take a reading every second
 }
 
+void photoResistor(){
+  lightLevel = analogRead(sensorPin);  // reads the voltage on the sensorPin
+  Serial.println(lightLevel);
+  delay(1000);
+  //autoRange();  // autoRanges the min / max values you see in your room.
 
+  calibratedlightLevel = map(lightLevel, 0, 1023, 0, 255);  // scale the lightLevel from 0 - 1023 range to 0 - 255 range.
+                                                  // the map() function applies a linear scale / offset.
+//  Serial.print(calibratedlightLevel);                                                // map(inputValue, fromMin, fromMax, toMin, toMax);
+  Serial.print("\t");       // tab character
+  Serial.print(calibratedlightLevel);   // println prints an CRLF at the end (creates a new line after)
+
+  analogWrite(ledPin, calibratedlightLevel);    // set the led level based on the input lightLevel.
+  if(lightLevel < 10)
+  {
+    digitalWrite(ledPin, LOW);
+  }
+  else
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+}
+
+void autoRange(){
+  if (lightLevel < minThreshold)  // minThreshold was initialized to 1023 -- so, if it's less, reset the threshold level.
+    minThreshold = lightLevel;
+
+  if (lightLevel > maxThreshold)  // maxThreshold was initialized to 0 -- so, if it's bigger, reset the threshold level.
+    maxThreshold = lightLevel;
+
+  // Once we have the highest and lowest values, we can stick them
+  // directly into the map() function.
+  // 
+  // This function must run a few times to get a good range of bright and dark values in order to work.
+
+  lightLevel = map(lightLevel, minThreshold, maxThreshold, 0, 255);
+  lightLevel = constrain(lightLevel, 0, 255);
